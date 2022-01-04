c1d795a42c048ac44e0fd65ae132ab28ecb8fdc0
abdallahr99 <abodyrody55@gmail.com>
2022-01-04 16:39:10 +0200

    firebase init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'rashedy-team',
+    appId: '1:996484237035:web:3a0b1b7c2353c97541a0ae',
+    storageBucket: 'rashedy-team.appspot.com',
+    apiKey: 'AIzaSyD1Hkic2uYcOajq2XelYEjg9ip2LaPw5a0',
+    authDomain: 'rashedy-team.firebaseapp.com',
+    messagingSenderId: '996484237035',
+    measurementId: 'G-PFZB3Z21NS',
+  },
   production: true
 };
