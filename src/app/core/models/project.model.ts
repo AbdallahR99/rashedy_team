c1fcd7c0668497e7fbab69f823372ed87eadfac7
abdallahr99 <abodyrody55@gmail.com>
2022-01-13 06:56:34 +0200

    projects

--- src/app/core/models/project.model.ts
+++ src/app/core/models/project.model.ts
@@ -1,7 +1,7 @@
 export interface Project {
   name: string;
   description: string;
-  imageUrl: string;
+  imagesUrl: string[];
   url?: string;
   github?: string;
   tags?: ('web' | 'mobile' | 'backend')[];
